9ab2775eeda472717e5606886eb8747488e325ab
vrrodgers <trulymissvicky1982@gmail.com>
2019-03-29 21:07:01 -0500

    added unique attribute table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_014154) do
+ActiveRecord::Schema.define(version: 2019_03_30_015837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_03_30_014154) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_coordinators_on_email", unique: true
   end
 
   create_table "enrollments", force: :cascade do |t|
@@ -68,6 +69,8 @@ ActiveRecord::Schema.define(version: 2019_03_30_014154) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["location"], name: "index_registries_on_location", unique: true
+    t.index ["name"], name: "index_registries_on_name", unique: true
   end
 
   add_foreign_key "coordinator_registries", "coordinators"
